37d5d080898f7695af6770451f30040f87f2f018
Dale Dunlop <Dalejohndunlop@gmail.com>
2024-04-01 22:59:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,37 +11,40 @@ provider "aws" {
   region = "eu-west-2"
 }
 resource "aws_instance" "splunk_server" {
-  ami = "splunk_AMI_9.0.9_2024-03-28_20-53-35-7b65de6c-5006-4ca2-bd75-fdba95ae5d9d
-ami-0e26cf8ef2ab56e1c"
-  instance_type = "t2.micro"
-  key_name = "splunk_server"
-  tags = {
-    Name = "splunk_server"
-  }
+  ami                         = "ami-0e26cf8ef2ab56e1c" #Specific for eu-west-2
+  instance_type               = "t2.Large"
+  key_name                    = "splunk_server"
+  tags                        = { Name = "splunk_server" }
   associate_public_ip_address = true
-  vpc_security_group_ids = ["aws_security_group.splunk_server.id"]
-  subnet_id = "aws_subnet.splunk_server.id"
+  vpc_security_group_ids      = [aws_security_group.splunk_server.id]
+  subnet_id                   = aws_subnet.splunk_server.id
 }
 
 resource "aws_security_group" "splunk_server" {
-    name_prefix = "splunk_group"
+  name_prefix = "splunk_group"
 
-    ingress {
-        from_port = 22 # ssh port
-        to_port =  22
-        protocol = "tcp"
-        cidr_blocks = ["<your-IP>/32"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["<Your-IP>/32"]
+    description = "SSH access from my IP"
+  }
 
-    ingress {
-        from_port = 8000 # splunk web port
-        to_port =  8000
-        protocol = "tcp"
-        cidr_blocks = ["<your-IP>/32"]
-    }
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["<Your-IP>/32"]
+    description = "Splunk web access from my IP"
+  }
 }
 
 resource "aws_subnet" "splunk_server" {
-    vpc_id = ""
-    cidr_block = "10.0.0.0/24"
+  vpc_id     = "vpc-086e70d08326f6d96"
+  cidr_block = "172.31.48.0/20"
+}
+
+output "splunk_server_public_ip" {
+  value = aws_instance.splunk_server.public_ip
 }
